90c63685ef0e46c0a54cfc3ee7b060a4a5a64b45
Gyan Lima <gyan.patrick1@gmail.com>
2023-07-06 18:06:10 -0300

    relacionando produto e categoria

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -1,17 +1,21 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_category")
-
 public class Category implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
@@ -21,6 +25,10 @@ public class Category implements Serializable{
 	private Long id;
 	
 	private String name;
+	
+	@JsonIgnore
+	@ManyToMany(mappedBy="categories")
+	private Set<Product> products = new HashSet<>();
 	
 	public Category () {
 		
@@ -63,6 +71,10 @@ public class Category implements Serializable{
 			return false;
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
+	}
+
+	public Set<Product> getProducts() {
+		return products;
 	}
 	
 	
