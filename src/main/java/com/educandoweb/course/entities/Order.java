987227291b475454397918aa903f2b2eeb82ed7d
Gyan Lima <gyan.patrick1@gmail.com>
2023-07-22 14:49:43 -0300

    CRUD de user e outras implementacoes

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -2,17 +2,22 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -33,6 +38,13 @@ public class Order implements Serializable{
 	@ManyToOne
 	@JoinColumn(name="client_id")
 	private User client;
+	
+	
+	@OneToMany(mappedBy="id.order")
+	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy="order",cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	public Order () {}
 
@@ -79,6 +91,29 @@ public class Order implements Serializable{
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+	
+	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
+	public Set <OrderItem> getItems() {
+		return items;
+	}
+	
+	public double getTotal() {
+		double sum = 0.0;
+		
+		for (OrderItem x: items) {
+			sum += x.getSubTotal();
+		}
+		
+		return sum;
 	}
 
 	@Override
